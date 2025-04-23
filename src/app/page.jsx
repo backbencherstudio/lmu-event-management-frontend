7f9124c5349b947717cb6b50e8533f2529fd6ab4
shur0v <shurov.0.00.0@gmail.com>
2025-04-23 17:51:21 +0600

    Solve

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Landingpage from "./(client)/client/_components/landingpage";
 import Menu from "./(client)/client/_components/menu";
 export default function Home() {
